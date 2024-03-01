778c91797d13c2df663087b7295a704de1bdc269
JBarrett3 <jamesbarrett@umass.edu>
2024-02-29 21:48:58 -0500

    tables

---
+++ Backend/tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Users(
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    radius INTEGER,
+    notify BOOLEAN,
+    bio VARCHAR(500)
+);
+
+CREATE TABLE Posts(
+    id VARCHAR(50) REFERENCES Users(id),
+    species VARCHAR(50),
+    quantity INTEGER,
+    comments VARCHAR(50),
+    dt TIMESTAMP
+);
