1bd3830237dd78ecfa2a945c05013cfb2a310931
arnavbh64 <132407744+arnavbh64@users.noreply.github.com>
2024-03-24 16:52:18 -0700

    Separated report page functionality

--- frontend/wildwhere/lib/settings.dart
+++ frontend/wildwhere/lib/settings.dart
@@ -42,13 +42,7 @@ class _SettingsPageState extends State<SettingsPage> {
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const ReportPage(),
-              ),
-            );
-          },
+          onPressed: () { },
         ),
       ),
       body: Container(
@@ -136,80 +130,3 @@ class SliderHelper extends StatelessWidget {
   }
 }
 
-class ReportPage extends StatelessWidget {
-  const ReportPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sighting Report'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Row(children: [
-              SizedBox(width: 20),
-              Icon(Icons.image_rounded, size: 70),
-              SizedBox(width: 20),
-              Expanded(
-                child: Text('Choose an image to upload',
-                    style: TextStyle(fontSize: 20)),
-              )
-            ]),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                const SizedBox(width: 10),
-                ElevatedButton(
-                    onPressed: () {}, child: const Text('Choose File')),
-                const SizedBox(width: 30),
-                Container(
-                    margin: const EdgeInsets.all(1.0),
-                    padding: const EdgeInsets.all(4.0),
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 2),
-                        shape: BoxShape.rectangle,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10.0))),
-                    child: Text('No file chosen'))
-              ],
-            ),
-            const SizedBox(height: 20),
-            textFields('Type of animal: '),
-            const SizedBox(height: 20),
-            textFields('Number of animals: '),
-            const SizedBox(height: 20),
-            textFields('Additional comments: '),
-            const SizedBox(height: 40),
-            Center(
-              child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.green),
-                  ),
-                  child: const Text('Submit',
-                      style: TextStyle(color: Colors.white))),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Row textFields(String title) {
-    return Row(
-      children: [
-        const SizedBox(width: 10),
-        Text(title, style: const TextStyle(fontSize: 16)),
-        const SizedBox(width: 10),
-        const SizedBox(
-          width: 200,
-          child: TextField(
-            decoration: InputDecoration(border: OutlineInputBorder()),
-          ),
-        ),
-      ],
-    );
-  }
-}
