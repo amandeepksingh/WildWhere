814865d33613682bc2bf7c42c4d3a77c46066e54
AndrewItIs <andrewli607@gmail.com>
2024-04-15 16:37:19 -0400

    changed it to only write pid on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,22 +8,23 @@ if(process.env.location == "local") {
 	console.log(`[server] Detected that you are running locally`);
 } else {
 	console.log(`[server] Detected that you are running on a server`);
+	//write pid to a file
+
+	const pidnum = `${process.pid}`;
+	fs.writeFile("pid.txt", pidnum, (err) => {
+	if(err) {
+		console.log("error writing pid to file");
+		return;
+	}
+		console.log("wrote pid to pid file");
+	});
+
 }
 
 //Creates server to listen. Sends requests from server to app
 const server = express();
 server.use(app);
 
-//write pid to a file
-
-const pidnum = `${process.pid}`;
-fs.writeFile("pid.txt", pidnum, (err) => {
-	if(err) {
-		console.log("error writing pid to file");
-		return;
-	}
-	console.log("wrote pid to pid file");
-});
 
 
 //Server listens
