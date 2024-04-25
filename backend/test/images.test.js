d636814ce6ebc09bab96346db68d57e1130b8413
JBarrett3 <jamesbarrett@umass.edu>
2024-04-25 16:47:16 -0400

    Image testing complete (only posts and users testing remains)

--- backend/test/images.test.js
+++ backend/test/images.test.js
@@ -2,30 +2,25 @@ const request = require('supertest');
 const assert = require('assert');
 const { describe } = require('node:test');
 const app = require('../src/app');
-const path = require('path')
-
+const logger = require('../src/logger');
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-//TODO configure automatic teardown functionality
+var pool;
+var poolObj = {
+    user: process.env.dbUser,
+    host: process.env.dbHost,
+    database: process.env.dbName,
+    password: process.env.dbPass,
+    port: process.env.dbPort,
+};
+if(process.env.location !== "local") poolObj.ssl = {rejectUnauthorized: false}; //for server pool
+pool = new Pool(poolObj);
 
-const pool = new Pool({
-         user: process.env.dbUser,
-         host: process.env.dbHost,
-         database: process.env.dbName,
-         password: process.env.dbPass,
-         port: process.env.dbPort,
-     	// ssl: {
-     	// 	rejectUnauthorized:process.env.rejectUnauthorized
-     	// } //used only on EC2
-     });
-
-async function teardown() { //TODO before each run. Using before() or after() seems to cause async issues
-         await pool.query("DELETE FROM users;")
-         await pool.query("DELETE FROM posts;")
-         //await pool.query("SELECT setval('posts_pid_seq', 1, true);")
-         //await pool.query("SELECT setval('users_uid_seq', 1, true);")
-     }
+async function teardown() {
+    await pool.query("DELETE FROM users;")
+    await pool.query("DELETE FROM posts;")
+}
     
 
 describe("IMAGES: test user image upload", () => {
@@ -33,39 +28,67 @@ describe("IMAGES: test user image upload", () => {
         await teardown();
 
         //create user
-        const resp1 = await request(app).post('/users/createUser')
-            .send('uid=ffd')
-        const uid = resp1.body.uid;
+        const uid = 'testUID'
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
         
         //add img
-        const resp2 = await request(app).post('/images/userProfilePic/upload')
-            .field('uid', uid)
-            .attach('img', 'test/testImages/test1.jpg')
-        assert.strictEqual(resp2.status, 200)
+        const resp1 = await request(app).post('/images/userProfilePic/upload').field('uid', uid).attach('img', 'test/testImages/test1.jpg')
+        assert.strictEqual(resp1.status, 200)
+        const url1 = resp1.body.message
+        assert.ok(url1.includes('http'), "valid url returned by upload")
         
         //check img in db
-        const resp3 = await request(app).get(`/users/selectUser?uid=${uid}`)
-        assert.strictEqual(resp3.body.message[0].imglink, resp2.body.message)
+        const resp2 = await request(app).get(`/users/selectUser?uid=${uid}`)
+        const url2 = resp2.body.message[0].imglink
+        assert.ok(url2.includes('http'), "valid url put to database by upload")
+
+        //check url from s3 matching one in db
+        assert.strictEqual(url1, url2, 'same url from upload put to database')
 
         //teardown
-        await request(app).delete('/images/userProfilePic/delete')
-            .field('uid', uid)
+        await request(app).delete('/images/userProfilePic/delete').send('uid', uid)
     });
 
     it("IMAGES: test user profile picture upload missing uid", async () => {
-        const resp = await request(app).post('/images/userProfilePic/upload')
-            .attach('img', 'test/testImages/test1.jpg')
+        const resp = await request(app).post('/images/userProfilePic/upload').attach('img', 'test/testImages/test1.jpg')
         assert.strictEqual(resp.status, 400)
         assert.strictEqual(resp.body.message, "uid is required")
     })
 
     it("IMAGES: test user upload missing img", async () => {
-        const resp = await request(app).post('/images/userProfilePic/upload')
-            .field('uid', 1)
+        const resp = await request(app).post('/images/userProfilePic/upload').field('uid', 1)
         assert.strictEqual(resp.status, 400)
         assert.strictEqual(resp.body.message, "img must be specified")
     })
 
+    it("IMAGES: test user override", async () => {
+        await teardown();
+
+        //create user
+        const uid = 'testUID'
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
+        
+        //add img
+        const resp1 = await request(app).post('/images/userProfilePic/upload').field('uid', uid).attach('img', 'test/testImages/test1.jpg')
+        assert.strictEqual(resp1.status, 200)
+        const url1 = resp1.body.message
+        assert.ok(url1.includes('http'), "valid url returned by upload")
+
+        //add img again
+        const resp2 = await request(app).post('/images/userProfilePic/upload').field('uid', uid).attach('img', 'test/testImages/test2.jpg')
+        assert.strictEqual(resp2.status, 200)
+        const url2 = resp2.body.message
+        assert.ok(url2.includes('http'), "valid url returned by upload")
+
+        const resp3 = await request(app).get(`/users/selectUser?uid=${uid}`)
+        const url3 = resp3.body.message[0].imglink
+        assert.ok(url3.includes('http'), "check img in db")
+
+        assert.strictEqual(url2, url3, 'check url from s3 matching one in db')
+
+        //teardown
+        await request(app).delete('/images/userProfilePic/delete').send('uid', uid)
+    });
 })
 
 describe("IMAGES: test post image upload", () => {
@@ -73,43 +96,65 @@ describe("IMAGES: test post image upload", () => {
         await teardown();
 
         //create user
-        const resp1 = await request(app).post('/users/createUser')
-            .send('uid=ffd')
-        const uid = resp1.body.uid;
-        const resp1b = await request(app).post('/posts/createPost')
-            .send('pid=ffd')
-            .send(`uid=${uid}`)
-            .send('coordinate=(0.0, 0.0)')
-        const pid = resp1b.body.pid;
-        
+        const uid = "testUID"
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
+        const pid = (await request(app).post('/posts/createPost').send(`uid=${uid}`).send('coordinate=(0.0, 0.0)')).body.pid
+
         //add img
-        const resp2 = await request(app).post('/images/postPic/upload')
-            .field('pid', pid)
-            .attach('img', 'test/testImages/test1.jpg')
-        assert.strictEqual(resp2.status, 200)
+        const resp1 = await request(app).post('/images/postPic/upload').field('pid', pid).attach('img', 'test/testImages/test1.jpg')
+        assert.strictEqual(resp1.status, 200)
+        const url1 = resp1.body.message
         
-        //check img in db
-        const resp3 = await request(app).get(`/posts/selectPost?pid=${pid}`)
-        assert.strictEqual(resp3.body.message[0].imglink, resp2.body.message)
+        const resp2 = await request(app).get(`/posts/selectPost?pid=${pid}`)
+        const url2 = resp2.body.message[0].imglink
+
+        assert.strictEqual(url1, url2, 'url from post matches url in db')
 
         //teardown
-        await request(app).delete('/images/postPic/delete')
-            .send(`pid=${pid}`)
+        await request(app).delete('/images/postPic/delete').send(`pid=${pid}`)
     });
 
-    it("IMAGES: test post profile picture upload missing uid", async () => {
-        const resp = await request(app).post('/images/postPic/upload')
-            .attach('img', 'test/testImages/test1.jpg')
+    it("IMAGES: test post profile picture upload missing pid", async () => {
+        const resp = await request(app).post('/images/postPic/upload').attach('img', 'test/testImages/test1.jpg')
         assert.strictEqual(resp.status, 400)
         assert.strictEqual(resp.body.message, "pid is required")
     })
 
     it("IMAGES: test post upload missing img", async () => {
-        const resp = await request(app).post('/images/postPic/upload')
-            .field('pid', 1)
+        const resp = await request(app).post('/images/postPic/upload').field('pid', 1)
         assert.strictEqual(resp.status, 400)
         assert.strictEqual(resp.body.message, "img must be specified")
     })
+
+    it("IMAGES: test post override", async () => {
+        await teardown();
+
+        //create user + post
+        const uid = 'testUID'
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
+        const pid = (await request(app).post('/posts/createPost').send(`uid=${uid}`).send('coordinate=(0.0, 0.0)')).body.pid
+
+        //add img
+        const resp1 = await request(app).post('/images/postPic/upload').field('pid', pid).attach('img', 'test/testImages/test1.jpg')
+        assert.strictEqual(resp1.status, 200)
+        const url1 = resp1.body.message
+        assert.ok(url1.includes('http'), "valid url returned by upload")
+
+        //add img again
+        const resp2 = await request(app).post('/images/postPic/upload').field('pid', pid).attach('img', 'test/testImages/test2.jpg')
+        assert.strictEqual(resp2.status, 200)
+        const url2 = resp2.body.message
+        assert.ok(url2.includes('http'), "valid url returned by upload")
+
+        const resp3 = await request(app).get(`/posts/selectPost?pid=${pid}`)
+        const url3 = resp3.body.message[0].imglink
+        assert.ok(url3.includes('http'), "check img in db")
+
+        assert.strictEqual(url2, url3, 'check url from s3 matching one in db')
+
+        //teardown
+        await request(app).delete('/images/postPic/delete').send('pid', pid)
+    });
 
 })
 
@@ -118,25 +163,19 @@ describe("IMAGES: test user delete image", () => {
         await teardown()
     
         //add user
-        const resp1 = await request(app).post('/users/createUser')
-            .send('uid=ffd')
-        const uid = resp1.body.uid;
+        const uid = 'testUID'
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
         
         //add img
-        const resp2 = await request(app).post('/images/userProfilePic/upload')
-            .field('uid', uid)
-            .attach('img', 'test/testImages/test1.jpg')
-        assert.strictEqual(resp2.status, 200)
+        await request(app).post('/images/userProfilePic/upload').field('uid', uid).attach('img', 'test/testImages/test1.jpg')
 
         //delete img
-        const resp3 = await request(app).delete('/images/userProfilePic/delete')
-            .send(`uid=${uid}`)
-        assert.strictEqual(resp3.status, 200)
-        assert.strictEqual(resp3.body.message, 'image delete successful')
+        const resp1 = await request(app).delete('/images/userProfilePic/delete').send(`uid=${uid}`)
+        assert.strictEqual(resp1.status, 200)
+        assert.strictEqual(resp1.body.message, 'image delete successful')
 
-        //check img in db
-        const resp4 = await request(app).get(`/users/selectUser?uid=${uid}`)
-        assert.strictEqual(resp4.body.message[0].imglink, null)
+        const resp2 = await request(app).get(`/users/selectUser?uid=${uid}`)
+        assert.strictEqual(resp2.body.message[0].imglink, null, "check img purged from db")
     })
 
     it("IMAGES: test user delete missing uid", async () => {
@@ -152,30 +191,21 @@ describe("IMAGES: test post delete image", () => {
         await teardown()
     
         //add user
-        const resp1 = await request(app).post('/users/createUser')
-            .send('uid=ffd')
-        const uid = resp1.body.uid;
-        const resp1b = await request(app).post('/posts/createPost')
-            .send('pid=ffd')
-            .send(`uid=${uid}`)
-            .send('coordinate=(0.0, 0.0)')
-        const pid = resp1b.body.pid;
+        const uid = 'testUID'
+        await request(app).post('/users/createUser').send(`uid=${uid}`)
+        const pid = (await request(app).post('/posts/createPost').send(`uid=${uid}`).send('coordinate=(0.0, 0.0)')).body.pid
         
         //add img
-        const resp2 = await request(app).post('/images/postPic/upload')
-            .field('pid', pid)
-            .attach('img', 'test/testImages/test1.jpg')
-        assert.strictEqual(resp2.status, 200)
+        await request(app).post('/images/postPic/upload').field('pid', pid).attach('img', 'test/testImages/test1.jpg')
 
         //delete img
-        const resp3 = await request(app).delete('/images/postPic/delete')
-            .send(`pid=${pid}`)
-        assert.strictEqual(resp3.status, 200)
-        assert.strictEqual(resp3.body.message, 'image delete successful')
+        const resp1 = await request(app).delete('/images/postPic/delete').send(`pid=${pid}`)
+        assert.strictEqual(resp1.status, 200)
+        assert.strictEqual(resp1.body.message, 'image delete successful')
 
         //check img in db
-        const resp4 = await request(app).get(`/posts/selectPost?pid=${pid}`)
-        assert.strictEqual(resp4.body.message[0].imglink, null)
+        const resp2 = await request(app).get(`/posts/selectPost?pid=${pid}`)
+        assert.strictEqual(resp2.body.message[0].imglink, null)
     })
 
     it("IMAGES: test post delete missing pid", async () => {
