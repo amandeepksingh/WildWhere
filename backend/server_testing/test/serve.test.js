725583b9ab90107ef2ebb75b9b288396f1ceedc3
Andrew Li <andrewli607@gmail.com>
2024-04-26 17:10:57 -0400

    added test foldwer within server testing

---
+++ backend/server_testing/test/serve.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const assert = require('assert');
+const { describe } = require('node:test');
+
+
+
+describe("selecting users", () => {
+    it("sanity check", async () => {
+      assert.strictEqual(1,1);
+    })
+
+});
+
