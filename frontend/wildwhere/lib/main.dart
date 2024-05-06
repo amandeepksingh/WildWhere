9abecd53c37634e097cadd0532368c7595fd2ba2
jcbeaumont22 <117319925+jcbeaumont22@users.noreply.github.com>
2024-05-05 23:28:47 -0400

    Removing auto signout

--- frontend/wildwhere/lib/main.dart
+++ frontend/wildwhere/lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseAuth.instance.signOut();
+  //await FirebaseAuth.instance.signOut();
   UserController.init(); //refresh user token
   Database db = Database();
   if (UserController.user != null) {
