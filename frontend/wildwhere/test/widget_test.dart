d63524184d761cc77e229c3492762ab115e9debc
Jacob Beaumont <jcbeaumont@umass.edu>
2024-05-06 23:26:36 -0400

    Fix

--- frontend/wildwhere/test/widget_test.dart
+++ frontend/wildwhere/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:wildwhere/login.dart';
 import 'package:wildwhere/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(homeScreen: Login()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
