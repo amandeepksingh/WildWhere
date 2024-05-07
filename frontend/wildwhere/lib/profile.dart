17e5eff52fc61615d239819c8a5f533c2df4bf7a
Jacob Beaumont <jcbeaumont@umass.edu>
2024-05-06 23:25:58 -0400

    Changed layout

--- frontend/wildwhere/lib/profile.dart
+++ frontend/wildwhere/lib/profile.dart
@@ -32,16 +32,18 @@ class _ProfilePageState extends State<Profile> {
   void loadProfileData() async {
     prefs = await SharedPreferences.getInstance();
     var fetchUid = prefs?.getString('uid');
-    if(fetchUid == null) {
+    if (fetchUid == null) {
       print("Error: uid is null");
       return;
     }
     user = await db.getCurrentUser(fetchUid);
     setState(() {
-        username = prefs?.getString('username') ?? 'Unknown user';
-        bio = (prefs?.getString('bio')?.isEmpty ?? true) ? 'Say something about yourself!' : prefs!.getString('bio');
-        email = prefs?.getString('email') ?? 'Unknown email';
-        imageLink = user!['imglink'] ?? '';
+      username = prefs?.getString('username') ?? 'Unknown user';
+      bio = (prefs?.getString('bio')?.isEmpty ?? true)
+          ? 'Say something about yourself!'
+          : prefs!.getString('bio');
+      email = prefs?.getString('email') ?? 'Unknown email';
+      imageLink = user!['imglink'] ?? '';
     });
   }
 
@@ -59,24 +61,25 @@ class _ProfilePageState extends State<Profile> {
       backgroundColor: const Color.fromARGB(255, 214, 249, 212),
       appBar: AppBar(
         title: Text('$username'),
-          titleTextStyle: const TextStyle(color: Colors.black87, fontSize: 22,fontWeight: FontWeight.w700),
+        titleTextStyle: const TextStyle(
+            color: Colors.black87, fontSize: 22, fontWeight: FontWeight.w700),
         leading: const BackButton(color: Colors.black87),
         actions: [
           IconButton(
             padding: const EdgeInsets.only(right: 10.0),
-            icon: const Icon(Icons.more_horiz_rounded),
+            icon: const Icon(Icons.edit_outlined),
             iconSize: 30.0,
             color: Colors.black87,
-            onPressed: () async{
+            onPressed: () async {
               bool? result = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => EditProfile(prefs: prefs!)),
-                    );
-                    // If 'result' is true, reload the profile data.
-                    if (result == true) {
-                      loadProfileData();
-                    }
+                context,
+                MaterialPageRoute(
+                    builder: (context) => EditProfile(prefs: prefs!)),
+              );
+              // If 'result' is true, reload the profile data.
+              if (result == true) {
+                loadProfileData();
+              }
             },
           )
         ],
@@ -109,17 +112,16 @@ class _ProfilePageState extends State<Profile> {
                           height: 120,
                           clipBehavior: Clip.antiAlias,
                           decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.grey,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black38,
-                                blurRadius: 6.0,
-                                spreadRadius: 1.0,
-                                offset: Offset(0, 5),
-                              )
-                            ]
-                          ),
+                              shape: BoxShape.circle,
+                              color: Colors.grey,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black38,
+                                  blurRadius: 6.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(0, 5),
+                                )
+                              ]),
                           child: (imageLink == null || imageLink == '')
                               ? Image.asset('assets/images/defaultpp.png',
                                   fit: BoxFit.cover)
@@ -158,12 +160,11 @@ class _ProfilePageState extends State<Profile> {
             ),
             const SizedBox(height: 5),
             Padding(
-              padding: const EdgeInsets.only(top: 3,bottom: 10),
-              child: Text(
-                "$username's Posts",
-                style: const TextStyle(fontSize: 18),
-                )
-              ),
+                padding: const EdgeInsets.only(top: 3, bottom: 10),
+                child: Text(
+                  "$username's Posts",
+                  style: const TextStyle(fontSize: 18),
+                )),
             Divider(
               color: Theme.of(context).brightness == Brightness.dark
                   ? Colors.white // Color for dark theme
@@ -183,24 +184,24 @@ class _ProfilePageState extends State<Profile> {
 
   Widget userPostsSection() {
     if (prefs == null) return const CircularProgressIndicator();
-      var uid = prefs!.getString('uid')!;
-      return FutureBuilder<List<Post>>(
-        future: db.getAllUserPosts(uid),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.data == null || snapshot.data!.isEmpty) {
-            return const Text('No Posts Yet');
-          } else {
-            return ListView.separated(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                Post post = snapshot.data![index];
-                return InkWell(
+    var uid = prefs!.getString('uid')!;
+    return FutureBuilder<List<Post>>(
+      future: db.getAllUserPosts(uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.data == null || snapshot.data!.isEmpty) {
+          return const Text('No Posts Yet');
+        } else {
+          return ListView.separated(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              Post post = snapshot.data![index];
+              return InkWell(
                   onTap: () {
                     Navigator.push(
                       context,
@@ -210,80 +211,82 @@ class _ProfilePageState extends State<Profile> {
                     );
                   },
                   child: Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: (
-                      const BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            spreadRadius: 0,
-                            blurRadius: 5.0,
-                            offset: Offset(0, 3),
-                          )
-                        ]
-                      )
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.only(left: 5, top: 5, bottom: 5),
-                          padding: const EdgeInsets.all(10),
-                          width: 130,
-                          height: 105,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(post.imgLink ?? 'https://via.placeholder.com/150'),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child : Padding(
-                            padding: const EdgeInsets.only(top: 7),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(post.animalName ?? 'Unknown Animal', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                                FutureBuilder<Placemark>(
-                                    future: getCityState(post.coordinate['y'], post.coordinate['x']),
-                                    builder: (context, snapshot) {
-                                      if (snapshot.connectionState == ConnectionState.waiting) {
-                                        return const Text("Loading location...");
-                                      } else if (snapshot.hasData) {
-                                        Placemark place = snapshot.data!;
-                                        return Text("Location: ${place.locality}, ${place.administrativeArea}");
-                                      } else {
-                                        return const Text("Location unknown");
-                                      }
-                                    },
-                                  ),
-                                Text('Quantity: ${post.quantity}'),
-                                Text('Activity: ${post.activity}')
-                              ],
-                            )
-                          )
+                      padding: const EdgeInsets.all(5),
+                      decoration:
+                          (const BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 0,
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3),
                         )
-                      ]
-                    )
-                  )
-                );
-              },
-              separatorBuilder: (context, index) => const SizedBox(height: 13),
-            );
-          }
-        },
-      );
-    }
+                      ])),
+                      child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  left: 5, top: 5, bottom: 5),
+                              padding: const EdgeInsets.all(10),
+                              width: 130,
+                              height: 105,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(post.imgLink ??
+                                      'https://via.placeholder.com/150'),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: Padding(
+                                    padding: const EdgeInsets.only(top: 7),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text(
+                                            post.animalName ?? 'Unknown Animal',
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold)),
+                                        FutureBuilder<Placemark>(
+                                          future: getCityState(
+                                              post.coordinate['y'],
+                                              post.coordinate['x']),
+                                          builder: (context, snapshot) {
+                                            if (snapshot.connectionState ==
+                                                ConnectionState.waiting) {
+                                              return const Text(
+                                                  "Loading location...");
+                                            } else if (snapshot.hasData) {
+                                              Placemark place = snapshot.data!;
+                                              return Text(
+                                                  "Location: ${place.locality}, ${place.administrativeArea}");
+                                            } else {
+                                              return const Text(
+                                                  "Location unknown");
+                                            }
+                                          },
+                                        ),
+                                        Text('Quantity: ${post.quantity}'),
+                                        Text('Activity: ${post.activity}')
+                                      ],
+                                    )))
+                          ])));
+            },
+            separatorBuilder: (context, index) => const SizedBox(height: 13),
+          );
+        }
+      },
+    );
+  }
+
   Future<Placemark> getCityState(double lat, double long) async {
     List<Placemark> placemarks = await placemarkFromCoordinates(lat, long);
     return placemarks.first;
+  }
 }
-
-
-}
-
