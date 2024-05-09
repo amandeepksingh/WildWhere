ffade4a3200f87bf089cc49c1fc2c74dfc89946b
Jacob Beaumont <jcbeaumont@umass.edu>
2024-05-09 00:11:36 -0400

    add geolocator plugin

--- frontend/wildwhere/lib/database.dart
+++ frontend/wildwhere/lib/database.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wildwhere/post.dart';
