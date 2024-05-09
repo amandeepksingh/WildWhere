0509f7b05bc54a625c6e9b243a342f3641fa71d5
arnavbh64 <132407744+arnavbh64@users.noreply.github.com>
2024-05-09 00:09:47 -0400

    added getPostsInRadius

--- frontend/wildwhere/lib/database.dart
+++ frontend/wildwhere/lib/database.dart
@@ -32,6 +32,26 @@ class Database {
     }
   }
 
+  Future<List<Post>> getPostsInRadius(int radius, Position coordinate) async {
+    var url = Uri.parse('$endpoint/posts/selectPost?coordinate=(${coordinate.longitude},${coordinate.latitude})&radius=$radius');
+    var response = await client.get(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      List<dynamic> postsJson = data['message'];
+      return postsJson.map((json) => Post.fromJson(json)).toList();
+    } else {
+      // Error handling if the request fails
+      throw Exception(
+          'Failed to fetch posts. Server responded with ${response.statusCode}: ${response.body}');
+    }
+  }
+  
   Future<List<Post>> getAllUserPosts(String uid) async {
     var url = Uri.parse('$endpoint/posts/selectPost?uid=$uid');
 
